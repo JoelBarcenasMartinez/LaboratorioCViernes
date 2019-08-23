0052698f4b5f29acfb918371b3281e7688cb369b
joel.barcenas <joel.barcenas@banregio.com>
2019-08-23 08:56:46 -0500

    Primer Commit Practica1

--- main.c
+++ main.c
@@ -4,5 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int numero = 1;
+	printf("El número es: %d", numero);
 	return 0;
 }
