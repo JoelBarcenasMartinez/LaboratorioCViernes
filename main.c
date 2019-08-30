06f0a30e5aa3ba73db66ebfc47efdeb3c387585a
Joel Barcenas <barcenasa07@gmail.com>
2019-08-30 07:57:57 -0500

    Revert "segundo commit"
    This reverts commit 549b0909760f163eda8e3768dd7b6b0b888592fd.

--- main.c
+++ main.c
@@ -4,6 +4,5 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	//Comentario de 2do commit.
 	return 0;
 }
